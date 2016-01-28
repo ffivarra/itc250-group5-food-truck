5e5deac9fad1d291894728dbeee9fb74f5750038
Luis Gamboa <luisgamboasierra@gmail.com>
2016-01-28 12:23:39 -0800

    add styling

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Our Food Truck</title>
+    <title>Ice Cream Order Online</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -18,12 +18,67 @@
 
 
 <!-----Main---------------------->
-<main>
-    <section class="layout">
+<div class="section group">
+
+    <div class="col span_1_of_2">
+    <h1>Ice Cream Order Online</h1>
+        <p>Welcome to our Ice Cream online page</p>
         
+        <form action="">
+            <p>Choose your flavor</p>
+            <input type="radio" name="choice" value="chocolate" > Chocolate<br>
+            <input type="radio" name="choice" value="bannana" > Bannana<br>
+            <input type="radio" name="choice" value="vanilla" > Vanilla<br>
+        </form>
         
-    </section>
-</main>
+        <br>
+        <br>
+        
+<tr>      
+    <td>
+            Choose a Quantity																						
+    </td>
+    <br>
+
+    <td>
+        <div>
+            <span>
+                <button type="button" data-index="1-1-4" data-sign="-">-</button>
+            </span>
+            <input id="" name="" value="0">
+            <span>
+                <button class="" type="button" data-index="1-1-4" data-sign="+">+</button>
+            </span>
+        </div>
+    </td>
+</tr>
+        
+<hr>
+        
+        <form action="">
+            <p>Choose your topping</p>
+            
+            <input type="checkbox" name="choice" value="oreo" > Oreo<br>
+            <input type="checkbox" name="choice" value="caramel" > Caramel<br>
+            <input type="checkbox" name="choice" value="fudge" > Fudge<br>
+               
+            <input type="checkbox" name="choice" value="nuts" > Nuts<br>
+            <input type="checkbox" name="choice" value="caramel" > Caramel<br>
+            <input type="checkbox" name="choice" value="fudge" > Fudge<br>
+            
+            <br>
+            
+            <button type="submit" name="submit" value="add">Add to Order</button>
+        </form>
+
+</div>
+    
+    <div class="col span_1_of_2">
+	Order Detail
+	</div>
+
+</div>
+    
 
 
 <!-----Footer-------------------->
